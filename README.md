5df374fb13a9f2c76c03c0b6a96802328565e64e
odenktools <odenktools86@gmail.com>
2018-02-25 23:05:34 +0700

    initialize code #4

---
+++ README.md
@@ -0,0 +1,7 @@
+# SpringBoot MicroService Sample
+
+
+```bash
+docker-compose up -d
+docker-compose up
+```
--- config-service/src/main/java/com/odenktools/configservice/ConfigServiceApp.java
+++ config-service/src/main/java/com/odenktools/configservice/ConfigServiceApp.java
@@ -3,7 +3,9 @@ package com.odenktools.configservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 
+@EnableAutoConfiguration
 @EnableConfigServer
 @SpringBootApplication
 public class ConfigServiceApp {
